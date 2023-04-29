b0931cb3ad4f464c61c0528c2b9b73428d9942b0
hamare1207 <110402773+hamare1207@users.noreply.github.com>
2023-04-29 22:24:39 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,37 +1,6 @@
 #include <math.h>
 #include "dxlib/DxLib.h"
-
-struct Window{
-	const static int WIDTH		= 640;
-	const static int HEIGHT		= 480;
-	const static int COLOR_BIT	= 16;
-};
-
-// 構造体, クラス宣言, プロトタイプ宣言 をここに
-
-class System{
-public:
-	int mx;
-	int my;
-	int key[256];
-	
-	int getKeys();
-	int getMouse();
-} sy;
-
-class Player{
-private:
-	int x;
-	int y;
-
-	const int s = 4;
-	
-public:
-	Player();
-	int player(int key[], int mx, int my);
-};
-
-// ここまで
+#include "main.hpp"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
 
--- main.hpp
+++ main.hpp
@@ -1,1 +1,29 @@
+// 構造体, クラス宣言, プロトタイプ宣言 をここに
 
+struct Window{
+	const static int WIDTH		= 640;
+	const static int HEIGHT		= 480;
+	const static int COLOR_BIT	= 16;
+};
+
+class System{
+public:
+	int mx;
+	int my;
+	int key[256];
+	
+	int getKeys();
+	int getMouse();
+} sy;
+
+class Player{
+private:
+	int x;
+	int y;
+
+	const int s = 4;
+	
+public:
+	Player();
+	int player(int key[], int mx, int my);
+};
